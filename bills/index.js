1cea895a7df7e191fae7ac78bd6f400289d9667e
Igor Bueno <igor@alphasoftware.br>
2017-12-06 18:04:37 -0400

    Final da tarde

--- bills/index.js
+++ bills/index.js
@@ -1,9 +1,12 @@
 import express from 'express';
 import create from './services/create';
+import remove from './services/delete';
 
 const router = express.Router();
 
 //localhost:3000/bills
 router.post('/',create);
+//localhost:3000/bills/:id
+router.delete('/:id',remove);
 
 export default router;
